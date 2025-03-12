a1466f728f6746be9e4c8a90af2d0b9be6e2d248
FrancescoRocca-001 <francescoduemilauno@gmail.com>
2025-03-12 17:48:55 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -17,3 +17,36 @@ plot(mato1992)
 im.plotRGB(mato1992,r=1,g=2,b=3)
 im.plotRGB(mato1992,r=2,g=1,b=3)
 im.plotRGB(mato1992,r=2,g=3,b=1)
+
+mato2006=im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006=flip(mato2006)
+plot(mato2006)
+
+im.plotRGB(mato2006,r=1,g=2,b=3)
+
+im.multiframe(1,2)
+im.plotRGB(mato1992,r=1,g=2,b=3)
+im.plotRGB(mato2006,r=1,g=2,b=3)
+
+im.multiframe(3,2)
+#NIR on top of red
+im.plotRGB(mato1992,r=1,g=2,b=3)
+im.plotRGB(mato2006,r=1,g=2,b=3)
+#NIR on top of green
+im.plotRGB(mato1992,r=2,g=1,b=3)
+im.plotRGB(mato2006,r=2,g=1,b=3)
+#NIR on top of blue
+im.plotRGB(mato1992,r=2,g=3,b=1)
+im.plotRGB(mato2006,r=2,g=3,b=1)
+
+dev.off()
+
+# exercise: plot only the first layer of mato2006
+plot(mato2006[[1]])
+plot(mato2006[[1]],col=magma(100))
+
+
+# DVI - Difference Vegetation Index
+# tree: NIR=255 (8bit), red=0, DVI=NIR-red=255-0=255
+# stressed tree: NIR=100, red=30, DVI=100-30=70
+
