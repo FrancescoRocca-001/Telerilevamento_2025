0a6131abe27ab102b8a887cad0b064cce89cce16
FrancescoRocca-001 <francescoduemilauno@gmail.com>
2025-03-20 15:54:13 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -1,8 +1,10 @@
 #R code for multitemporal analysis
 
+install.packages("ggridges")   #needed to create ridgeline plots
 library(terra)
 library(imageRy)
 library(viridis)
+library(ggridges)   #non servirebbe perchè lo richiama im.ridgeline
 
 im.list()
 
@@ -30,3 +32,40 @@ plot(grdif)
 
 
 
+#ridgeline plots
+
+im.ridgeline(gr, scale=1)
+im.ridgeline(gr, scale=2)
+im.ridgeline(gr, scale=2, palette="inferno")
+
+im.list()
+#exercise: import the NDVI data from sentinel
+ndvi=im.import("Sentinel2")
+im.ridgeline(ndvi,scale=2)   #male!
+
+#changing names
+#sources     : Sentinel2_NDVI_2020-02-21.tif  
+#             Sentinel2_NDVI_2020-05-21.tif  
+#             Sentinel2_NDVI_2020-08-01.tif  
+#             Sentinel2_NDVI_2020-11-27.tif  
+
+names(ndvi)=c("02_feb","05_may","08_aug","11_nov")
+im.ridgeline(ndvi,scale=2)
+im.ridgeline(ndvi,scale=2, palette="mako")
+
+
+pairs(ndvi)
+
+plot(ndvi[[1]],ndvi[[2]])
+# y=x --> may=feb
+# y = a + bx
+# a=0, b=1 --> y=x
+abline(0,1, col="red")
+plot(ndvi[[1]],ndvi[[2]],xlim=c(-0.3,0.9),ylim=c(-0.3,0.9))
+abline(0,1, col="red")
+
+im.multiframe(1,3)
+plot(ndvi[[1]])
+plot(ndvi[[2]])
+plot(ndvi[[1]],ndvi[[2]],xlim=c(-0.3,0.9),ylim=c(-0.3,0.9))
+abline(0,1, col="red")
