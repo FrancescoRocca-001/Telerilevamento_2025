0f6243e8e776d869cd674178d19a75535ccab28b
FrancescoRocca-001 <francescoduemilauno@gmail.com>
2025-04-10 15:04:06 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -5,9 +5,7 @@ library(imageRy)
 library(viridis)
 library(patchwork)
 install.packages("RStoolbox")
-install.packages("sf")
 library(RStoolbox)
-library(sf)
 
 # 24 26 25
 M=(24+26+25)/3
@@ -65,3 +63,52 @@ p_sd3+p_sd5
 #plot the original dev and the stdev
 p_nir=im.ggplot(nir)
 p_nir + p_sd3
+
+
+
+
+#what to do in case of huge images
+sent=im.import("sentinel.png")
+sent=flip(sent)
+plot(sent)
+
+ncell(sent)*nlyr(sent)
+
+sent_agg=aggregate(sent, fact=2)
+ncell(sent_agg)*nlyr(sent_agg)
+
+sent_agg5=aggregate(sent, fact=5)
+ncell(sent_agg5)*nlyr(sent_agg5)
+
+#exercise: make a multiframe and plot in RGB the 3 images
+im.multiframe(1,3)
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(sent_agg, r=1, g=2, b=3)
+im.plotRGB(sent_agg5, r=1, g=2, b=3)
+
+#calculating standard deviation
+dev.off()
+nir_agg=sent_agg[[1]]
+sd3_agg=focal(nir_agg, w=c(3,3), fun="sd")
+plot(sd3_agg)
+
+#exercise: calculate the standard deviation for the factor 5 image
+nir_agg5=sent_agg5[[1]]
+sd3_agg5=focal(nir_agg5, w=c(3,3), fun="sd")
+plot(sd3_agg5)
+
+sd5_agg5=focal(nir_agg5, w=c(5,5), fun="sd")
+plot(sd5_agg5)
+
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3_agg)
+plot(sd3_agg5)
+plot(sd5_agg5)
+
+p1=im.ggplot(sd3)
+p2=im.ggplot(sd3_agg)
+p3=im.ggplot(sd3_agg5)
+p4=im.ggplot(sd5_agg5)
+p1+p2+p3+p4
